2ba979422ae077c5ac1cc8982816f9452969b13b
rushikesh <rushikeshvadnal7@gmail.com>
2023-01-30 16:31:08 +0530

    email updated

--- footer.js
+++ footer.js
@@ -13,7 +13,7 @@ function sendEmail() {
         Host: "smtp.elasticemail.com",
         Username: "rushikeshvadnal7@gmail.com",
         Password: "350319117806CC0FA0C9EFA9A91A8462A699",
-        To: 'kingdhruv42512@gmail.com',
+        To: 'thejerseyfcstore@gmail.com',
         From: "rushikeshvadnal7@gmail.com",
         Subject: "FEEDBACK",
         Body: `
